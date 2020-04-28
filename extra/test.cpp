461ee16350dd1daf1e3f94ce7e17bae948d5a60a
berwingan <wqg203@nyu.edu>
2020-04-28 14:51:07 +0400

    a

---
+++ extra/test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <omp.h>
+#include <algorithm>
+#include <math.h>
+#include <map>
+#include <string>
+#include <ctime>
+using namespace std;
+
+#define NUM 10
+
+void openMP()
+{
+    omp_set_num_threads(1);
+    int sum = 0;
+#pragma omp parallel for shared(sum)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            sum++;
+        }
+    }
+    cout << "sum = " << sum << endl;
+}
+int main()
+{
+    cout << "Open MP \n";
+    openMP();
+    return 0;
+}
